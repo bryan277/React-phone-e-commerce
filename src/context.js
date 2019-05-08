bb0312a073fb1330591f683060981ebb7cb841d3
hfvos90 <hfvos90@gmail.com>
2019-05-08 15:10:28 -0700

    #

--- src/context.js
+++ src/context.js
@@ -12,10 +12,17 @@ class ProductProvider extends Component {
   handleDetail = () => {
     console.log('hello from detail');
   }
+  addToCart = () => {
+    console.log('hello from add to cart');
+  }
+  
   render() {
     return (
-      <ProductContext.Provider value={{
-
+      <ProductContext.Provider 
+        value={{
+        ...this.state, 
+        handleDetail: this.handleDetail,
+        addToCart: this.addToCart
       }}>
         {this.props.children}
       </ProductContext.Provider>
@@ -27,3 +34,5 @@ class ProductProvider extends Component {
 const ProductConsumer = ProductContext.Consumer;
 
 export { ProductProvider, ProductConsumer };
+
+
