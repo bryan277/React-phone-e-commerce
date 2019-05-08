3312300e0353901733d1676546c37f8d312e491e
hfvos90 <hfvos90@gmail.com>
2019-05-08 13:23:12 -0700

    Updates

---
+++ precache-manifest.d23cd204888a1359ef80d68d884b13bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b21b474fd571b3debcc2d84f3706aa",
+    "url": "/React-phone-e-commerce/index.html"
+  },
+  {
+    "revision": "c56cecbaf3764a5cba3c",
+    "url": "/React-phone-e-commerce/static/css/2.81b134f3.chunk.css"
+  },
+  {
+    "revision": "8c00cad9f2cb5fb4dade",
+    "url": "/React-phone-e-commerce/static/css/main.1cdbd4d9.chunk.css"
+  },
+  {
+    "revision": "c56cecbaf3764a5cba3c",
+    "url": "/React-phone-e-commerce/static/js/2.e1ffa10c.chunk.js"
+  },
+  {
+    "revision": "8c00cad9f2cb5fb4dade",
+    "url": "/React-phone-e-commerce/static/js/main.be6edcc6.chunk.js"
+  },
+  {
+    "revision": "286bcf33cbbbf75f970d",
+    "url": "/React-phone-e-commerce/static/js/runtime~main.6c964212.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/React-phone-e-commerce/static/media/logo.9f7e506d.svg"
+  }
+]);
